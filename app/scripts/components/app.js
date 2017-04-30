b90ae1ef135693e6dc2a0aa36e8ba7424a65afe1
Doubleu Gee <Waleed.gamal1000@gmail.com>
2017-04-30 02:35:15 +0200

    define tabs component and mounted into dom

---
+++ app/scripts/components/app.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Tabs from './Tabs'
+
+
+class App extends React.Component{
+  render(){
+    return(
+      <Tabs />
+    )
+  }
+}
+
+module.exports = App
--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,3 +2,8 @@ import sass from '../styles/main.sass'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import prop from 'prop-types'
+import App from './components/app'
+
+ReactDOM.render(<App/>,
+    document.getElementById('app')
+)
