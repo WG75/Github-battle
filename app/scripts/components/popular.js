d587ad1cf054bfd1c088de6a24c442b09918f5fd
Doubleu Gee <Waleed.gamal1000@gmail.com>
2017-05-02 15:52:20 +0200

    add <Repo/> component that take the ajax response as its props and dislay the data into the page

--- app/scripts/components/popular.js
+++ app/scripts/components/popular.js
@@ -22,10 +22,33 @@ const Tabs = (props) => {
        )
 }
 
+const Repos = (props) => {
+  return(
+    <ul className="repos-wrapper">
+      {props.repos.map((repo, i) => {
+        return(
+          <li key={repo.id} className='repo'>
+            <span>{'#' + (i + 1)}</span>
+            <img src={repo.owner.avatar_url} alt={repo.description}/>
+            <a href={repo.url}>{repo.name}</a>
+            <spna>{'@' + repo.owner.login}</spna>
+            <span>{repo.stargazers_count + ' stars'}</span>
+          </li>
+
+        )
+      })}
+    </ul>
+  )
+}
+
 
 Tabs.propTypes = {
   updateActiveTab: prop.func.isRequired,
   activeTab: prop.string.isRequired
+}
+
+Repos.propTypes = {
+  repos: prop.array.isRequired
 }
 
 
@@ -45,7 +68,7 @@ class Popular extends React.Component{
       repos: null
     })
 
-    api.fetshRepos(this.state.activeTab)
+    api.fetshRepos(activeTab)
     .then((res) => {
       this.setState({
         repos: res
@@ -55,8 +78,14 @@ class Popular extends React.Component{
 
   render(){
       return (
+        <div>
           <Tabs updateActiveTab={this.updateActiveTab}
                 activeTab={this.state.activeTab} />
+          {!this.state.repos ?
+          <p>loading....</p> :
+          <Repos repos={this.state.repos} />
+          }
+        </div>
              )
        }
 
